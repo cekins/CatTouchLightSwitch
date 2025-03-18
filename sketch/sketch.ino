75b43573227b5860b8c88c3bcb5b74a2eaf3bcdb
ChandlerLaptop <chandlerekins@gmail.com>
2025-03-18 01:14:07 -0400

    created first full version

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,57 +1,57 @@
 //TODO
-#ifdef SERIAL_MODE
-  #include <SoftwareSerial.h>
-  #define RX 3
-  #define TX 4
-  SoftwareSerial Serial(RX, TX);
-#endif
-
 #include <ADCTouch.h>
+#include "tinyServo85.h"
 
 #define STARTUP_DELAY 5000
 
+#define SERVO_PIN 1
+#define TOUCH_PIN 1 //this is analog "1" not the same as the digital pin
+
+#define TOUCH_SAMPLE_PERIOD 50
+#define TOUCH_THRESHOLD 50
+
+#define MILLI_PER_DEGREE 50
+#define SERVO_NEUTRAL_POSITION 0
+#define SERVO_LIGHT_OFF_POSITION 90
+#define SERVO_MOVE_TIME 500
+// I think the library requires these but Im not sure
+#define SVOMINPULSE 1000
+#define SVOMAXPULSE 2000
 
 
-#define PAD 1 //This is not the same "1" as the digital pin
-#define TOUCH_THRESHOLD 70
-bool is_pad_touched() {
-  static int last_touch_value = ADCTouch.read(PAD, __AVR_ATtiny85__);
-  return ADCTouch.read(PAD, __AVR_ATtiny85__) - last_touch_value > TOUCH_THRESHOLD;
+tinyServo85 servo;
+
+int get_touch_value() {
+  return ADCTouch.read(TOUCH_PIN, __AVR_ATtiny85__);
 }
 
+bool is_pad_touched() {
+  static int initial_touch_value = get_touch_value();
+  return get_touch_value > initial_touch_value + TOUCH_THRESHOLD;
+}
 
-#define SERVO 1
 void turn_off_light_switch() {
-  analogWrite(SERVO, 200);
-  delay(500);
-  analogWrite(SERVO, 150);
-  delay(5000);
+  servo.setServo(SERVO_PIN, SERVO_LIGHT_OFF_POSITION);
+  delay(SERVO_MOVE_TIME);
+  servo.setServo(SERVO_PIN, SERVO_NEUTRAL_POSITION);
 }
 
 void setup() 
 {
+  // setup servo
+  servo.setCTC();
+  servo.attachServo(SERVO_PIN);
+  servo.homeServos();
 
-  pinMode(SERVO, OUTPUT);
-  digitalWrite(SERVO, LOW);
-
-  //Delay to give time to move hand away
-  delay(STARTUP_DELAY);
-
-#ifdef SERIAL_MODE
-  Serial.begin(9600);
-  Serial.println("starting");
-#endif
+  // setup touch
+  delay(STARTUP_DELAY); // give user time to move hand away
+  is_pad_touched();
 } 
 
-#define SAMPLE_PERIOD 50
 void loop() 
 { 
   if (is_pad_touched()) {
-    #ifdef SERIAL_MODE
-      Serial.begin(9600);
-      Serial.println("starting");
-    #endif
     turn_off_light_switch();
   }
-  delay(SAMPLE_PERIOD);
+  delay(TOUCH_SAMPLE_PERIOD);
 }
