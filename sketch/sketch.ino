4a803dc5c8a3cdcefd8e1e20d3f2ddfeaabbb787
chandler <chandlerekins@gmail.com>
2025-03-31 19:47:55 -0400

    final sketch

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -7,13 +7,13 @@
 #define SERVO_PIN 1
 #define TOUCH_PIN 1 //this is analog "1" not the same as the digital pin
 
-#define TOUCH_SAMPLE_PERIOD 50
-#define TOUCH_THRESHOLD 50
+#define TOUCH_SAMPLE_PERIOD 10
+#define TOUCH_DELTA 30
+#define SAMPLE_AVERAGE_NUM 4
 
-#define MILLI_PER_DEGREE 50
+#define MILLI_PER_DEGREE 20
 #define SERVO_NEUTRAL_POSITION 0
 #define SERVO_LIGHT_OFF_POSITION 90
-#define SERVO_MOVE_TIME 500
 // I think the library requires these but Im not sure
 #define SVOMINPULSE 1000
 #define SVOMAXPULSE 2000
@@ -26,14 +26,37 @@ int get_touch_value() {
 }
 
 bool is_pad_touched() {
-  static int initial_touch_value = get_touch_value();
-  return get_touch_value > initial_touch_value + TOUCH_THRESHOLD;
+  static int threshold = get_touch_value() + TOUCH_DELTA;
+  static int last_values[SAMPLE_AVERAGE_NUM];
+  static int i = 0;
+  static int sum = 0;
+
+  sum -= last_values[i];
+  last_values[i] = get_touch_value();
+  sum += last_values[i];
+
+  if (++i == SAMPLE_AVERAGE_NUM)
+    i = 0;
+
+  return (sum / SAMPLE_AVERAGE_NUM) > threshold;
+}
+
+void move_servo(int pos) {
+  static int last_pos = pos;
+  do {
+    if (last_pos > pos)
+      last_pos--;
+    else if (last_pos < pos)
+      last_pos++;
+    servo.setServo(SERVO_PIN, last_pos);
+    delay(MILLI_PER_DEGREE);
+  } while(last_pos != pos);
 }
 
 void turn_off_light_switch() {
-  servo.setServo(SERVO_PIN, SERVO_LIGHT_OFF_POSITION);
-  delay(SERVO_MOVE_TIME);
-  servo.setServo(SERVO_PIN, SERVO_NEUTRAL_POSITION);
+  move_servo(SERVO_LIGHT_OFF_POSITION);
+  //delay(SERVO_MOVE_TIME);
+  move_servo(SERVO_NEUTRAL_POSITION);
 }
 
 void setup() 
@@ -41,11 +64,14 @@ void setup()
   // setup servo
   servo.setCTC();
   servo.attachServo(SERVO_PIN);
-  servo.homeServos();
+  move_servo(SERVO_NEUTRAL_POSITION);
 
   // setup touch
   delay(STARTUP_DELAY); // give user time to move hand away
-  is_pad_touched();
+  
+  // initialize all values of static array
+  for(int i; i < SAMPLE_AVERAGE_NUM; i++)
+    is_pad_touched();
 } 
 
 void loop() 
@@ -53,5 +79,5 @@ void loop()
   if (is_pad_touched()) {
     turn_off_light_switch();
   }
-  delay(TOUCH_SAMPLE_PERIOD);
+  //delay(TOUCH_SAMPLE_PERIOD);
 }
